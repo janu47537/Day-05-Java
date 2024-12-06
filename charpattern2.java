1ed8f8e78e5103b8715110646cf57bfbac5aa409
janu47537 <janu47537@gmail.com>
2024-12-06 15:24:11 +0530

    Add files via upload

---
+++ charpattern2.java
@@ -0,0 +1,24 @@
+/*
+6
+A 
+A B
+A B C
+A B C D
+A B C D E
+A B C D E F
+*/
+import java.util.*;
+public class charpattern2{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print((char)(64+j)+" ");
+            }
+System.out.println();
+        }
+    }
+}
